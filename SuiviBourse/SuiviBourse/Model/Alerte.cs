d58c85e3f52e075a1f27137561188ed0d55a8359
mamobileagency <pcottaz@hotmail.com>
2018-04-15 18:28:07 +0200

    all in a Grid

--- SuiviBourse/SuiviBourse/Model/Alerte.cs
+++ SuiviBourse/SuiviBourse/Model/Alerte.cs
@@ -14,12 +14,33 @@ namespace SuiviBourse.Model
             Code = code;
             Libelle = libelle;
         }
+
+       
+        public Alerte(string code, string libelle , float variation , float alerteHCours, float alerteBCours, float alerteHVar, float alerteBVar)
+        {
+            Code = code;
+            Libelle = libelle;
+            Variation = variation;
+            AlerteHCours = alerteHCours;
+            AlerteBCours =  alerteBCours;
+            AlerteHVar= alerteHVar;
+            AlerteBVar= alerteBVar;
+        }
         public string Code { get => code; set => code = value; }
         public string Libelle { get => libelle; set => libelle = value; }
         public float Cours { get => cours; set => cours = value; }
 
+        public float Variation { get; set; }
+        public float AlerteHCours { get; set; }
+        public float AlerteBCours { get; set; }
+        public float AlerteHVar { get; set; }
+        public float AlerteBVar { get; set; }
+
+
         private string code;
         private string libelle;
         private float cours;
+
+
     }
 }
